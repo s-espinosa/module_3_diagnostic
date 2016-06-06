33bdbb2abe7c087d121befef1439e4d01ba8c288
Sal Espinosa <sespinos@gmail.com>
2016-06-06 10:30:08 -0600

    Work through pagination

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   def index
-    @stations = NRELService.find_stations(params[:q])
+    page = params[:page]
+    if params[:distance] == ""
+      stations = NRELService.find_stations(params[:q], params[:distance])
+    else
+      stations = NRELService.find_stations(params[:q], params[:distance], "all")
+    end
+    # would like to put this into a hash/presenter
+    @stations = stations[(page.to_i-1)*10..(page.to_i*10)-1]
+    @pages = stations.count / 10
+    @zip = params[:q]
+    @distance = params[:distance]
   end
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,6 +1,5 @@
 class NRELService
-  def self.find_stations(zip_code)
-    json = JSON.parse(Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&format=JSON&location=80203&radius=6&fuel_type=ELEC,LPG").body)['fuel_stations']
-    byebug
+  def self.find_stations(zip_code, distance = "6.0", fuel_type = "ELEC,LPG")
+    json = JSON.parse(Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&format=JSON&location=#{zip_code}&radius=#{distance}&fuel_type=#{fuel_type}").body)['fuel_stations']
   end
 end
