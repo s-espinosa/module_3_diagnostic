2c5249947598623f4279a039a7b20ae03f456ca9
Sal Espinosa <sespinos@gmail.com>
2016-06-06 10:10:29 -0600

    Update link to properly use distance

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,5 +1,6 @@
 class NRELService
   def self.find_stations(zip_code)
-    json = JSON.parse(Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_key']}&format=JSON&location=80203&radius=6&fuel_type=ELEC,LPG&limit=10").body)['fuel_stations']
+    json = JSON.parse(Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&format=JSON&location=80203&radius=6&fuel_type=ELEC,LPG").body)['fuel_stations']
+    byebug
   end
 end
--- spec/features/user_can_find_stations_with_optional_distance_spec.rb
+++ spec/features/user_can_find_stations_with_optional_distance_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "When a user searches by zip" do
+RSpec.describe "When a user searches by zip with a distance" do
   it "a list of stations is presented" do
-    VCR.use_cassette("search_by_zip") do
+    VCR.use_cassette("search_with_limited_distance") do
       visit '/'
       fill_in "q", with: "80203"
+      fill_in "distance", with: "5"
       click_on("Locate")
 
       expect(page).to have_content("Name")
@@ -12,6 +13,9 @@ RSpec.describe "When a user searches by zip" do
       expect(page).to have_content("Fuel Types")
       expect(page).to have_content("Distance")
       expect(page).to have_content("Access Times")
+      within(pagination) do
+        expect(page).to have_content("7")
+      end
     end
   end
 end
